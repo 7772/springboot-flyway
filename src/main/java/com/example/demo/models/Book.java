06dc563685447890be5866144215f3e402f9e0a8
landon <landon@olulo.io>
2021-04-25 12:20:16 +0900

    record - 14

--- src/main/java/com/example/demo/models/Book.java
+++ src/main/java/com/example/demo/models/Book.java
@@ -3,8 +3,10 @@ package com.example.demo.models;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "books")
 public class Book {
     @Id
     @Column
@@ -20,11 +22,23 @@ public class Book {
         this.id = id;
     }
 
+    public Long getId() {
+        return id;
+    }
+
     public void setName(String name) {
         this.name = name;
     }
 
+    public String getName() {
+        return name;
+    }
+
     public void setAuthor(String author) {
         this.author = author;
+    }
+
+    public String getAuthor() {
+        return author;
     }
 }
--- src/test/java/com/example/demo/models/BookTest.java
+++ src/test/java/com/example/demo/models/BookTest.java
@@ -1,16 +1,41 @@
 package com.example.demo.models;
 
+import java.util.Optional;
+
 import com.example.demo.ResetDatabase;
+import com.example.demo.daos.BookDao;
 import org.junit.jupiter.api.Test;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class BookTest extends ResetDatabase {
+    @Autowired
+    private BookDao bookDao;
+
     @Test
     public void createSuccess() {
-        // 1. Test DB를 바라봐야 함.
-        // 2. 테스트 수행을 할 때마다, DB가 초기화되어야 함. (데이터의 상태에 따라 테스트 결과가 달라지는 것을 방지하기 위해)
-        // 3. Book 개체가 DB에 생성되는지 테스트.
+        // Given
+        Book book = new Book();
+        book.setId((long) 1);
+        book.setName("flyway 정복하기");
+        book.setAuthor("김저자");
+
+        // When
+        bookDao.save(book);
+
+        // Then
+        Optional<Book> bookFromDB = bookDao.findById((long) 1);
+
+        assertThat(bookFromDB.isPresent()).isTrue();
+
+        assertThat(bookFromDB.get().getId()).isEqualTo(book.getId());
+        assertThat(bookFromDB.get().getName()).isEqualTo(book.getName());
+        assertThat(bookFromDB.get().getAuthor()).isEqualTo(book.getAuthor());
     }
 }
